257b60a39124a1dc8ddd7516511ea1ef480ecf89
Naren Venkataraman <naren@doorul.com>
2013-09-23 20:27:43 -0400

    Fix up test

--- geo/haversine_test.go
+++ geo/haversine_test.go
@@ -1,15 +1,19 @@
-package geo 
+package geo
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
+
+func check(t* testing.T, want string, lat1, lon1, lat2, lon2 float64) {
+	if got := fmt.Sprintf("%.2f", Haversine(lat1, lon1, lat2, lon2)); got != want {
+		t.Errorf("Haversine = got %f; want %f", got, want)
+	}
+
+}
 
 func TestHaverine(t *testing.T) {
 	//distance b/w lyon and paris
-	if got, want := 391.970466; Haversine(45.7597, 4.8422, 48.8567, 2.3508) != want {
-		t.Errorf("Havesine = %f; want %f", got, want)
-	}
-
-	if got, want := 2884.632202; Haversine(36.12, -86.67, 33.94, -118.40) != want {
-		t.Errorf("Havesine = %f; want %f", got, want)
-	}
-
+	check(t, "391.97", 45.7597, 4.8422, 48.8567, 2.3508)
+	check(t, "2884.63", 36.12, -86.67, 33.94, -118.40)
 }
